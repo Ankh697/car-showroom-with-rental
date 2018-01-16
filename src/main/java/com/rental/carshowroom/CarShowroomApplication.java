e0eb94e9e6d90d2a6fa47e4a1f29894775341466
Kazz1211 <kazurreth1211@gmail.com>
2018-01-16 09:29:11 +0100

    Initial commit

---
+++ src/main/java/com/rental/carshowroom/CarShowroomApplication.java
@@ -0,0 +1,12 @@
+package com.rental.carshowroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarShowroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarShowroomApplication.class, args);
+	}
+}
