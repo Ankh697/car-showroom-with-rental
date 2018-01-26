72b4fab99466cf8d5564c1c0c6406225280f9954
Ankh697 <marcin.zawadzki1@pollub.edu.pl>
2018-01-26 08:42:22 +0100

    lease controller + entity

--- src/main/java/com/rental/carshowroom/controller/LeasingController.java
+++ src/main/java/com/rental/carshowroom/controller/LeasingController.java
@@ -1,7 +1,5 @@
 package com.rental.carshowroom.controller;
 
-import com.rental.carshowroom.model.Leasing;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/api/leasing")
